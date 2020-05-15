38a1c7f8044faac5b9adc15c53b7226cdf78d081
Erwan31 <spilmonterwan@gmail.com>
2020-05-15 21:16:21 +0100

    Login page basic

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Movies from './movies'
-import { Link, Switch, Route, Redirect, NavLink } from 'react-router-dom'
+import { Switch, Route, Redirect} from 'react-router-dom'
 import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import NavBar from './components/navBar';
 import MovieForm from './components/movieForm';
+import LoginForm from './components/loginForm';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
    <main className="container">
     <NavBar />
     <Switch>
+      <Route path="/login" component={LoginForm} />
       <Route path="/movies/:id" component={MovieForm} />
       <Route path="/movies" render={ () => <Movies />} />
       <Route path="/customers" component={Customers} />
